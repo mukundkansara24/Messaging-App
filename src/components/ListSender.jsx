1c4e298298e7f27499a6571843c5153adb690836
Mukund Kansara <mukundkansara2411@gmail.com>
2026-03-16 22:44:28 +0530

    Improve listSender and Message Page UI

--- src/components/ListSender.jsx
+++ src/components/ListSender.jsx
@@ -51,6 +51,55 @@ function ListSender() {
     dispatch(setReceiver({ senderId }));
   }
   return (
+    // <> </> does not have layout property so i use div
+    <>
+      <div className="w-full h-full p-2">
+        <label className="input mb-2 pr-0">
+
+          <input type="search" placeholder="Search User"
+            value={newUser}
+            onChange={(e) => {
+              SetNewUser(e.target.value);
+            }}
+          />
+          <div className="btn btn-ghost"
+            onClick={(e) => {
+              searchUser();
+            }}
+          >
+            <svg className="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+          </div>
+        </label>
+        <ul className="list rounded-box">
+          {sender?.map((send) => (
+            <li className="list-row hover:bg-base-100 m-1" key={send.rows[0]?.$id}
+              onClick={(e) => listMessage(send.rows[0]?.$id)}
+            >
+              <div>{send.rows[0]?.Username}</div>
+            </li>
+          ))}
+
+        </ul>
+      </div>
+    </>
+  );
+}
+
+export default ListSender;
+
+
+/*
     <>
       <div className="flex-1">
         {sender?.map((send) => (
@@ -74,15 +123,9 @@ function ListSender() {
         />
         <button
           className="flex-none w-8 h-8 p-1 border rounded-lg hover:bg-gray-300"
-          onClick={(e) => {
-            searchUser();
-          }}
         >
           <img src={SearchLogo} alt="search" />
         </button>
       </div>
     </>
-  );
-}
-
-export default ListSender;
+*/
--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,17 +3,23 @@ import ListSender from "./ListSender";
 import MessageList from "./MessageList";
 function Message() {
   return (
-    <>
-      <div className="flex h-full w-full">
-        <div className="flex flex-col h-full w-1/3 p-2 m-1 mt-3 rounded-lg border">
-          <ListSender />
-        </div>
-        <div className="h-full w-2/3 m-1 mt-3 rounded-lg border">
-          <MessageList />
-        </div>
-      </div>
-    </>
+    <div className="flex w-full h-full">
+      <div className="card m-1 bg-base-300 w-1/3 rounded-box grid place-items-center"><ListSender /></div>
+      <div className="divider divider-horizontal m-0"></div>
+      <div className="card m-1 w-2/3 bg-base-300 rounded-box grid place-items-center">content</div>
+    </div>
   );
 }
-1 / 3;
 export default Message;
+
+
+// <>
+//   <div className="flex h-full w-full">
+//     <div className="flex flex-col h-full w-1/3 p-2 m-1 mt-3 rounded-lg border">
+//       <ListSender />
+//     </div>
+//     <div className="h-full w-2/3 m-1 mt-3 rounded-lg border">
+//       <MessageList />
+//     </div>
+//   </div>
+// </>
